ecaef1d3c50ff2347768ce4e199f3164b908f019
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-02 16:32:49 +0100

    js update

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
 button.addEventListener('click', function(event) {
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const minLength = 8;
+  // const email = emailInput.value.trim();
+  // const password = passwordInput.value.trim();
+  // const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // const minLength = 8;
 
-  if (emailPattern.test(email) && password.length >= minLength) {
-    alert('Form submitted successfully!');
-  } else {
-    alert('Invalid email or password. Please check your inputs.');
-  }
+  // if (emailPattern.test(email) && password.length >= minLength) {
+  //   alert('Form submitted successfully!');
+  // } else {
+  //   alert('Invalid email or password. Please check your inputs.');
+  // }
 });
   
